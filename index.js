e40065c6b18fc1d608e29cf387b2a62c42450b16
ebaauw <erik.baauw@xs4all.nl>
2018-01-12 14:32:19 +0100

    Consistency with homebridge-music

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 // homebridge-zp/index.js
 // Copyright © 2016-2018 Erik Baauw. All rights reserved.
 //
-// Homebridge plug-in for Sonos ZonePlayer.
+// Homebridge plugin for Sonos ZonePlayer.
 
 'use strict';
-
-const dynamic = false;
 
 const ZPPlatformModule = require('./lib/ZPPlatform');
 const ZPPlatform = ZPPlatformModule.ZPPlatform;
 
-module.exports = function(homebridge) {
+module.exports = (homebridge) => {
   ZPPlatformModule.setHomebridge(homebridge);
-  homebridge.registerPlatform('homebridge-zp', 'ZP', ZPPlatform, dynamic);
+  homebridge.registerPlatform('homebridge-zp', 'ZP', ZPPlatform);
 };
