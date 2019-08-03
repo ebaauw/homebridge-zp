a3f20d7c8925ddf7afb93e9e22961ba2f4832acf
ebaauw <erik.baauw@xs4all.nl>
2019-08-03 12:07:05 +0200

    Bump dependencies
    - NodeJS v10.16.1 LTS;
    - homebridge-lib v4.4.1-0.
    
    Include command-line tools from homebridge-lib.

---
+++ cli/hap.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+// homebridge-zp/cli/hap.js
+// Copyright © 2019 Erik Baauw. All rights reserved.
+//
+// Homebridge plugin for Sonos ZonePlayer.
+
+'use strict'
+
+const homebridgeLib = require('homebridge-lib')
+
+new homebridgeLib.HapCommand().main()
---
+++ cli/json.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+// homebridge-zp/cli/json.js
+// Copyright © 2019 Erik Baauw. All rights reserved.
+//
+// Homebridge plugin for Sonos ZonePlayer.
+
+'use strict'
+
+const homebridgeLib = require('homebridge-lib')
+
+new homebridgeLib.JsonCommand().main()
