89583bb4369f61daf22ed18557ea5a64e0aa0a76
ebaauw <erik.baauw@xs4all.nl>
2020-01-01 12:26:30 +0100

    Happy New Year

--- cli/zp.js
+++ cli/zp.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // homebridge-zp/cli/zp.js
-// Copyright © 2019 Erik Baauw. All rights reserved.
+// Copyright © 2019-2020 Erik Baauw. All rights reserved.
 //
 // Homebridge plugin for Sonos ZonePlayer.
 
