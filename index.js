81849e354d5471e62648032e7b9e4486e152da2d
ebaauw <erik.baauw@xs4all.nl>
2021-07-18 16:27:14 +0200

    Update index.js
    - No longer register ZPS1 platform, see #153.

--- index.js
+++ index.js
@@ -5,10 +5,9 @@
 
 'use strict'
 
-const { ZpPlatform, ZpS1Platform } = require('./lib/ZpPlatform')
+const ZpPlatform = require('./lib/ZpPlatform')
 const packageJson = require('./package.json')
 
 module.exports = (homebridge) => {
   ZpPlatform.loadPlatform(homebridge, packageJson, 'ZP', ZpPlatform)
-  ZpPlatform.loadPlatform(homebridge, packageJson, 'ZPS1', ZpS1Platform)
 }
